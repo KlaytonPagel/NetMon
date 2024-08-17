4578c4a769992b26e7ad87009789468f599e461b
Klayton Pagel <pagelk@lopers.unk.edu>
2024-08-17 05:16:12 -0500

    Created A Setup Script
    Created a bash script that will do most of the setup for you, you just have to set up the configuration.

---
+++ netmon/netmon_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+working_dir="/opt/netmon"
+if [ "$PWD" != "$working_dir" ]; then
+    mkdir -p "$working_dir"
+    mv main.py $working_dir
+    mv netmon.cnf $working_dir
+    touch "$working_dir/.env"
+    mv netmon.service /etc/systemd/system
+else
+    touch "$working_dir/.env"
+    mv netmon.service /etc/systemd/system
+fi
+
+systemctl daemon-reload
+systemctl enable netmon
